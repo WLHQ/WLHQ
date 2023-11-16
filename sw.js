b08b3417e6b154b0892ff482ccd45eae60f6603b
WLHQ <106452668+WLHQ@users.noreply.github.com>
2023-11-15 21:19:46 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const staticCacheName = 'site-static-pwa-v1.0.9';
-const dynamicCacheName = 'site-dynamic-pwa-v1.0.9';
+const staticCacheName = 'site-static-pwa-v1.1.0';
+const dynamicCacheName = 'site-dynamic-pwa-v1.1.0';
 const assets = [
 	'/WLHQ/',
 	'/WLHQ/index.html'
